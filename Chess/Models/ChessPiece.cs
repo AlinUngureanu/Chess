8dbb3e8bf1d79d508eaf2c11954ae986d7c46191
Ionut-Alin_Ungureanu <u.ionutalin@yahoo.com>
2023-05-21 23:49:51 +0300

    Add code

---
+++ Chess/Models/ChessPiece.cs
@@ -0,0 +1,24 @@
+﻿namespace Chess.Models
+{
+    public enum PieceColor
+    {
+        White,
+        Black
+    }
+    public enum ChessPieceType
+    {
+        King,
+        Queen,
+        Rook,
+        Bishop,
+        Knight,
+        Pawn
+    }
+    public class ChessPiece
+    {
+        public PieceColor Color { get; set; }
+        public ChessPieceType Type { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
